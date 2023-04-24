1d4360ade45e4976ddfcafa2e5957eff9d2b3519
xzyt <zerofreezex@gmail.com>
2023-04-24 21:57:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,24 +20,26 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "role" {
+resource "aws_iam_role" "iam_role_discord_bot" {
+  name = "destracker-iam-role-discord-bot"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-  name               = var.name
+  inline_policy {
+    name = "destracker-iam-policy-discord-bot"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["ssm:GetParameter"]
+          Effect   = "Allow"
+          Resource = aws_ssm_parameter.parameter_discord_bot_token.arn
+        },
+      ]
+    })
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "basic" {
+resource "aws_iam_role_policy_attachment" "lambda_basic_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.role.name
-}
-
-resource "aws_iam_role_policy_attachment" "additional" {
-  count      = length(var.attachments)
-  policy_arn = element(var.attachments, count.index)
-  role       = aws_iam_role.role.name
-}
-
-resource "aws_iam_role_policy" "inline" {
-  count  = length(var.inline_policies)
-  policy = element(var.inline_policies, count.index)
-  role   = aws_iam_role.role.id
 }
